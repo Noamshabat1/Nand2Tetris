c9daf32414c93e29e5ec6e870b94e9e1f7b589fd
Noam Shabat <92057540+Noamshabat1@users.noreply.github.com>
2024-01-23 19:26:49 +0200

    Update Main.py

--- Project's/project 6/Main.py
+++ Project's/project 6/Main.py
@@ -13,61 +13,96 @@ from Parser import Parser
 from Code import Code
 
 
-def assemble_file(input_file: typing.TextIO, output_file: typing.TextIO) -> None:
+def format_binary_command(address, bit_length=16):
+    """
+    Formats an address to a binary command string of specified Bit length.
+
+    """
+    binary_command = bin(int(address)).replace("0b", "")
+    return binary_command.rjust(bit_length, '0')
+
+
+def assemble_file(input_file: typing.TextIO,
+                  output_file: typing.TextIO) -> None:
     """Assembles a single file.
 
     Args:
         input_file (typing.TextIO): the file to assemble.
         output_file (typing.TextIO): writes all output to this file.
     """
-    s_table = SymbolTable()  # initialization
+    symbol_table_container = [SymbolTable()]
+    parser_container = [Parser(input_file)]
+    process_first_pass(parser_container, symbol_table_container)
+    process_second_pass(parser_container, symbol_table_container, output_file)
 
-    #  First Pass
+
+def process_first_pass(parser_container, symbol_table_container):
+    """Processes the first pass of the assembler and also processes the
+    L_COMMAND type instructions.
+    """
     address = 0
-    parser_1 = Parser(input_file)
-    while parser_1.has_more_commands():
-        if parser_1.command_type() == "L_COMMAND":
-            symbol = parser_1.symbol()
-            s_table.add_entry(symbol, address)
+    while parser_container[0].has_more_commands():
+        if parser_container[0].command_type() == "L_COMMAND":
+            var = parser_container[0].symbol()
+            symbol_table_container[0].add_entry(var, address)
         else:
             address += 1
-        parser_1.advance()
-    parser_1.restart()
+        parser_container[0].advance()
+    parser_container[0].restart()
 
-    #  Second Pass
-    variable_available_address = 16
-    while parser_1.has_more_commands():
-        if parser_1.command_type() == "A_COMMAND":
-            symbol = parser_1.symbol()
-            if not symbol.isdigit():
-                if not s_table.contains(symbol):
-                    s_table.add_entry(symbol, variable_available_address)
-                    variable_available_address += 1
 
-                decimal_address = s_table.get_address(symbol)
-                command = bin(int(decimal_address)).replace("0b", "")
-                output_file.write("0" * (16 - len(command)) + command + "\n")
-            else:
-                command = bin(int(symbol)).replace("0b", "")
-                while len(command) != 15:
-                    command = "0" + command
-                output_file.write("0" + command + "\n")
-        elif parser_1.command_type() == "C_COMMAND":
-            dest = parser_1.dest()
-            comp = parser_1.comp()
-            is_shift = False
-            if ">>" in comp or "<<" in comp:
-                is_shift = True
-            jump = parser_1.jump()
-            dest = Code.dest(dest)
-            comp = Code.comp(comp)
-            jump = Code.jump(jump)
-            if is_shift:
-                command = f"101{comp}{dest}{jump}"
-            else:
-                command = f"111{comp}{dest}{jump}"
-            output_file.write(command + "\n")
-        parser_1.advance()
+def process_second_pass(parser_container, symbol_table_container, output_file):
+    """Processes the second pass of the assembler.
+    """
+    variable_address = [16]
+    while parser_container[0].has_more_commands():
+        command_type = parser_container[0].command_type()
+        if command_type == "A_COMMAND":
+            process_A_command(parser_container, symbol_table_container,
+                              variable_address, output_file)
+
+        elif command_type == "C_COMMAND":
+            process_C_command(parser_container, output_file)
+
+        parser_container[0].advance()
+
+
+def process_A_command(parser_container, symbol_table_container,
+                      variable_open_address, output_file):
+    """Processes A_COMMAND type instructions.
+    """
+    symbol = parser_container[0].symbol()
+    if not symbol.isdigit():
+        if not symbol_table_container[0].contains(symbol):
+            symbol_table_container[0].add_entry(symbol,
+                                                variable_open_address[0])
+            variable_open_address[0] += 1
+
+        address = symbol_table_container[0].get_address(symbol)
+        command = bin(int(address)).replace("0b", "")
+        output_file.write("0" * (16 - len(command)) + command + "\n")
+        
+    else:
+        command = bin(int(symbol)).replace("0b", "")
+        while len(command) != 15:
+            command = "0" + command
+        output_file.write("0" + command + "\n")
+
+
+def process_C_command(parser_container, output_file):
+    """Processes C_COMMAND type instructions.
+    """
+    dest, comp, jump = (parser_container[0].dest(),
+                        parser_container[0].comp(),
+                        parser_container[0].jump())
+    is_shift = ">>" in comp or "<<" in comp
+
+    dest = Code.dest(dest)
+    comp = Code.comp(comp)
+    jump = Code.jump(jump)
+
+    command_prefix = "101" if is_shift else "111"
+    output_file.write(f"{command_prefix}{comp}{dest}{jump}\n")
 
 
 if "__main__" == __name__:
@@ -78,17 +113,18 @@ if "__main__" == __name__:
     # correct path, using the correct filename.
     if not len(sys.argv) == 2:
         sys.exit("Invalid usage, please use: Assembler <input path>")
-
     argument_path = os.path.abspath(sys.argv[1])
     if os.path.isdir(argument_path):
-        files_to_assemble = [os.path.join(argument_path, filename) for filename in os.listdir(argument_path)]
+        files_to_assemble = [
+            os.path.join(argument_path, filename)
+            for filename in os.listdir(argument_path)]
     else:
         files_to_assemble = [argument_path]
-
     for input_path in files_to_assemble:
         filename, extension = os.path.splitext(input_path)
         if extension.lower() != ".asm":
             continue
         output_path = filename + ".hack"
-        with open(input_path, 'r') as input_file, open(output_path, 'w') as output_file:
+        with open(input_path, 'r') as input_file, \
+                open(output_path, 'w') as output_file:
             assemble_file(input_file, output_file)
